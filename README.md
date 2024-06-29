7c566997ca422b1f482852f5871b782a7a5794e7
Xuski10 <Xuski_Tave_10@hotmail.com>
2024-06-29 13:01:18 +0200

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# order_it_2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/order_it_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.order_it_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
